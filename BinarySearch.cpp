2082d7bbf8034cb8c552cd908a362330fcb25363
iizaghz <sukmahawaiza@gmail.com>
2025-05-07 12:15:56 +0700

    add condition statement

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -11,5 +11,12 @@ void input()
     {
         cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
         cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else{
+            cout << "\n[!] Jumlah elemen tidak boleh lebih daari 10. Silahkan coba lagi.\n";
+        }
     }
 }
